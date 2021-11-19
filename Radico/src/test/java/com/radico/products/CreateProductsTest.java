3b22bd1dffde3780398af56b99362610ae7237d7
jyothisrikanth <jyothisrikanth@DESKTOP-TM4S8S0>
2021-11-19 15:46:36 +0530

    First commit
    Upload all files

---
+++ Radico/src/test/java/com/radico/products/CreateProductsTest.java
@@ -0,0 +1,14 @@
+package com.radico.products;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateProductsTest {
+
+	@Test
+	public void createProducts()
+	{
+		Reporter.log("Products Created successfully",true);
+		//Raghav Test Jenkins
+	}
+}
